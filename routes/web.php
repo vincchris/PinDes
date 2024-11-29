0f98a8605d8fb045710b3e7a04613b2dd4306753
aldiui <aldijaya280902@gmail.com>
2024-11-29 19:32:52 +0700

    fixed home page

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,17 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/HomePage', [HomeController::class, 'index'])->name('home');
-Route::get('/edukasi', [EdukasiController::class, 'index'])->name('edukasi');
-Route::get('/konektivitas', [KonektivitasController::class, 'index'])->name('konektivitas');
-Route::get('/pelatihan', [UmkmController::class, 'index'])->name('pelatihan');
-Route::get('/telemedicine', [TelemedicineController::class, 'index'])->name('telemedicine');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Other routes for authenticated users
+    Route::get('/edukasi', [EdukasiController::class, 'index'])->name('edukasi');
+    Route::get('/konektivitas', [KonektivitasController::class, 'index'])->name('konektivitas');
+    Route::get('/pelatihan', [UmkmController::class, 'index'])->name('pelatihan');
+    Route::get('/telemedicine', [TelemedicineController::class, 'index'])->name('telemedicine');
 });
 
 require __DIR__ . '/auth.php';
